498eb90e39aee244e18351c11bea443bffe60e5c
Andrew Durber <aidurber@gmail.com>
2021-08-28 08:09:58 +0000

    feat(settings): add min and max header depth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   Plugin,
   PluginSettingTab,
   Setting,
+  Notice,
 } from "obsidian";
 import { DEFAULT_SETTINGS } from "./constants";
 import { ContentsTableRenderer } from "./ContentsTableRenderer";
@@ -29,7 +30,6 @@ export default class DynamicTOCPlugin extends Plugin {
     element: HTMLElement,
     _: MarkdownPostProcessorContext
   ) {
-    console.log(this.settings);
     this.renderer = new ContentsTableRenderer(
       this.app,
       source,
@@ -40,15 +40,17 @@ export default class DynamicTOCPlugin extends Plugin {
     this.renderer.build(this.app.workspace.getActiveFile());
     this.app.metadataCache.on("changed", this.renderer.build);
   }
+
+  rerenderToC = () => {
+    if (!this.renderer) return;
+    this.renderer.build(this.app.workspace.getActiveFile());
+  };
   loadSettings = async () => {
-    const data = await this.loadData();
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, data);
-    console.log({ DEFAULT_SETTINGS, settings: this.settings });
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
   };
 
   saveSettings = async () => {
     await this.saveData(this.settings);
-    console.log("Save settings", this.settings);
   };
 }
 
@@ -66,32 +68,53 @@ class DynamicTOCSettingsTab extends PluginSettingTab {
     containerEl.empty();
 
     containerEl.createEl("h2", { text: "Dynamic Table of Contents Settings" });
-
-    console.log("Before render settings", this.plugin.settings);
     new Setting(containerEl)
       .setName("List Style")
       .setDesc("The table indication")
-      .addDropdown((cb) => {
-        cb.setValue(this.plugin.settings.style);
-        cb.addOptions({ bullet: "Bullet", number: "Number" });
-        cb.onChange((val) => {
-          console.log(val);
-          this.plugin.settings.style = val as BulletStyle;
-          this.plugin.saveData(this.plugin.settings);
-          console.log("Saving settings");
-        });
-      });
-
-    // Just testing to see if toggle sets value correctly
-    new Setting(containerEl)
-      .setName("Use numeric indicators")
-      .addToggle((toggle) =>
-        toggle
-          .setValue(this.plugin.settings.style === "number")
+      .addDropdown((cb) =>
+        cb
+          .addOptions({ bullet: "Bullet", number: "Number" })
+          .setValue(this.plugin.settings.style)
           .onChange(async (val) => {
-            console.log(val);
-            this.plugin.settings.style = val === true ? "number" : "bullet";
-            this.plugin.saveData(this.plugin.settings);
+            this.plugin.settings.style = val as BulletStyle;
+            await this.plugin.saveData(this.plugin.settings);
+            this.plugin.rerenderToC();
+          })
+      );
+    new Setting(containerEl)
+      .setName("Minimum Header Depth")
+      .setDesc("The default minimum header depth to render")
+      .addSlider((slider) =>
+        slider
+          .setLimits(1, 6, 1)
+          .setValue(this.plugin.settings.min_depth)
+          .setDynamicTooltip()
+          .onChange(async (val) => {
+            if (val > this.plugin.settings.max_depth) {
+              new Notice("Min Depth is higher than Max Depth");
+            } else {
+              this.plugin.settings.min_depth = val;
+              await this.plugin.saveData(this.plugin.settings);
+              this.plugin.rerenderToC();
+            }
+          })
+      );
+    new Setting(containerEl)
+      .setName("Maximum Header Depth")
+      .setDesc("The default maximum header depth to render")
+      .addSlider((slider) =>
+        slider
+          .setLimits(1, 6, 1)
+          .setValue(this.plugin.settings.max_depth)
+          .setDynamicTooltip()
+          .onChange(async (val) => {
+            if (val < this.plugin.settings.min_depth) {
+              new Notice("Max Depth is higher than Min Depth");
+            } else {
+              this.plugin.settings.max_depth = val;
+              await this.plugin.saveData(this.plugin.settings);
+              this.plugin.rerenderToC();
+            }
           })
       );
   }
